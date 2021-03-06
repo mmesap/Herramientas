13f1ec18cf5c64f1995fd85432734865fc57da5b
mmesap <54986519+mmesap@users.noreply.github.com>
2021-03-06 18:21:31 -0500

    Update and rename s to HelloWorld.cpp

---
+++ 2021-02-26/HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+}*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+  int a = 0;
+  int b = 0;
+  std::cin >> a ;
+  b = 2 * a;
+  std::cout << "Hello World!\n";
+  std::cout << "2 * " << a <<" : " << b << std::endl;
+  return 0;
+}
